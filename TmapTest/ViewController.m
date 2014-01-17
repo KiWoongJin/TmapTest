b1aaa3e6a480af378388b7b6775b965ad645821d
KiWoongJin <jkw87first@naver.com>
2014-01-17 14:24:58 +0900

    TmapTest

--- TmapTest/ViewController.m
+++ TmapTest/ViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "TMapView.h"
+
+#define APP_KEY @"9808e4e1-db20-3f1f-8b3c-77606b3b71a1"
 
 @interface ViewController ()
 
@@ -18,6 +21,28 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    TMapView *mapView = [[TMapView alloc]initWithFrame:self.view.bounds];
+    [mapView setSKPMapApiKey:APP_KEY];
+    
+    CLLocationCoordinate2D coord[5] = {
+        CLLocationCoordinate2DMake(37.460143, 126.914062),
+        CLLocationCoordinate2DMake(37.469136, 126.981869),
+        CLLocationCoordinate2DMake(37.437930, 126.989937),
+        CLLocationCoordinate2DMake(37.413255, 126.959038),
+        CLLocationCoordinate2DMake(37.426752, 126.913548)
+    };
+    
+    TMapPolygon *polygon = [[TMapPolygon alloc]init];
+    [polygon setLineColor:[UIColor redColor]];
+    
+    [polygon setPolygonAlpha:0];
+    [polygon setLineWidth:8.0];
+    
+    for (int i = 0; i <5; i++) {
+        [polygon addPolygonPoint:[TMapPoint mapPointWithCoordinate:coord[i]]];
+    }
+    [mapView addTMapPolygonID:@"관악산" Polygon:polygon];
+    [self.view addSubview:mapView];
 }
 
 - (void)didReceiveMemoryWarning
